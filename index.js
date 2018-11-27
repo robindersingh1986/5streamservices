268b064802827ec9dc12b22a07cc836da0c5d9b1
ROBINDER SINGH <rob@ROBINDERs-MacBook-Pro.local>
2018-11-27 09:13:41 +0530

    added express favicon

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const favicon = require('express-favicon');
+
 const app = express()
 const port = 3000
 
+app.use(favicon(__dirname + '/public/assets/images/logo_30_30.png'));
 app.use(express.static('public'))
 //app.get('/', (req, res) => res.send('Hello World!'))
 
