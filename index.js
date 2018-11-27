c86c10567ba2f0e22eb1ead8a09711d490b106da
ROBINDER SINGH <rob@ROBINDERs-MacBook-Pro.local>
2018-11-27 09:26:20 +0530

    added test route

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const express = require('express')
-const favicon = require('express-favicon');
+// const favicon = require('express-favicon');
 
 const app = express()
 const port = 3000
 
-function ignoreFavicon(req, res, next) {
-    if (req.originalUrl === '/favicon.ico') {
-      res.status(204).json({nope: true});
-    } else {
-      next();
-    }
-  }
-
-app.use(ignoreFavicon);
 // app.use(favicon(__dirname + '/public/assets/images/logo_30_30.png'));
 app.use(express.static('public'))
+app.get('/favicon.ico', (req, res) => res.status(204).json({nope: true}))
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
