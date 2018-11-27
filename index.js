f64d50c4e3a10bae2ca78364ba1b3a14e53e0c8e
ROBINDER SINGH <rob@ROBINDERs-MacBook-Pro.local>
2018-11-27 09:20:00 +0530

    added code to ignore favicon

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const favicon = require('express-favicon');
 const app = express()
 const port = 3000
 
-app.use(favicon(__dirname + '/public/assets/images/logo_30_30.png'));
+function ignoreFavicon(req, res, next) {
+    if (req.originalUrl === '/favicon.ico') {
+      res.status(204).json({nope: true});
+    } else {
+      next();
+    }
+  }
+
+app.use(ignoreFavicon);
+// app.use(favicon(__dirname + '/public/assets/images/logo_30_30.png'));
 app.use(express.static('public'))
-//app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
